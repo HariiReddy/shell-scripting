239b4ce1548a9faa75f2f61667dfc3fb0e57ad8b
Harinath Reddy <harinath.reddy@impelsys.com>
2025-06-25 22:56:46 +0530

    Hello world

---
+++ 02.conversation.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Sachin: Virat, your consistency under pressure is phenomenal. Proud of how far you’ve come."
+echo "Kohli: Thank you, Sachin Paaji. Your guidance has always been my foundation."
+echo "Sachin: Do you remember your first match under my captaincy?"
+echo "Kohli: "Absolutely! I was nervous, but seeing you at the other end calmed me instantly."
