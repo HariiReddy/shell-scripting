e71a4317055e220fbcd791b82a13fc28733c75fd
Harinath Reddy <harinath.reddy@impelsys.com>
2025-06-25 23:04:21 +0530

    Hello world

---
+++ 03.variables.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+person1=$person1
+person2=$person2
+echo "$person1: Virat, your consistency under pressure is phenomenal. Proud of how far you’ve come."
+echo "$person2: Thank you, $person1 Paaji. Your guidance has always been my foundation."
+echo "$person1: Do you remember your first match under my captaincy?"
+echo "$person2: "Absolutely! I was nervous, but seeing you at the other end calmed me instantly."
