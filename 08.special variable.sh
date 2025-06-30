8523179eecbc59f07decb63f7375a065afbfa24c
Harinath Reddy <harinath.reddy@impelsys.com>
2025-06-30 21:30:42 +0530

    1

--- 08.special variable.sh
+++ 08.special variable.sh
@@ -2,3 +2,10 @@
 
 echo "All variables passed to script: $@"
 echo "Number of variables: $#"
+echo "script name: $0"
+echo "current directory: $PWD"
+echo "user runnig this script: $USER"
+echo " Home directory of user: $HOME"
+echo " PID of the script: $$"
+echo " PID of last command running in background: $!"
+echo " The exit status of the loast command executed: $?"
